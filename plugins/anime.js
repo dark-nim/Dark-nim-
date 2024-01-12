b7a10b0c25a45e54171c601f2bcfa9cf2d9f1e84
inrl-official <fasweehmon2@gmail.com>
2024-01-12 22:41:45 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -13,7 +13,7 @@ inrl({
 	}, async (message, match, cmd) => {
 		const url = await getJson(`${config.BASE_URL}api/anime/husbu?apikey=${config.INRL_KEY}`);
 		if (!url.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`);
-		return await message.send(await getBuffer(url.result), {}, 'image');
+		return await message.send(await getBuffer(url.result.url), {}, 'image');
 	});
 inrl({
 	pattern: 'loli ?(.*)',
@@ -22,7 +22,7 @@ inrl({
 }, async (message, match, cmd) => {
 	const url = await getJson(`${config.BASE_URL}api/anime/loli?apikey=${config.INRL_KEY}`);
 	if (!url.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`);
-	return await message.send(await getBuffer(url.result), {}, 'image');
+	return await message.send(await getBuffer(url.result.url), {}, 'image');
 });
 inrl({
 	pattern: 'neko ?(.*)',
@@ -31,7 +31,7 @@ inrl({
 }, async (message, match, cmd) => {
 	const url = await getJson(`${config.BASE_URL}api/anime/neko?apikey=${config.INRL_KEY}`);
 	if (!url.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`);
-	return await message.send(await getBuffer(url.result), {}, 'image');
+	return await message.send(await getBuffer(url.result.url), {}, 'image');
 });
 inrl({
 	pattern: 'shota ?(.*)',
@@ -40,7 +40,7 @@ inrl({
 }, async (message, match, cmd) => {
 	const url = await getJson(`${config.BASE_URL}api/anime/shota?apikey=${config.INRL_KEY}`);
 	if (!url.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`);
-	return await message.send(await getBuffer(url.result), {}, 'image');
+	return await message.send(await getBuffer(url.result.url), {}, 'image');
 });
 inrl({
 	pattern: 'wifu ?(.*)',
@@ -49,5 +49,5 @@ inrl({
 }, async (message, match, cmd) => {
 	const url = await getJson(`${config.BASE_URL}api/anime/wifu?apikey=${config.INRL_KEY}`);
 	if (!url.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`);
-	return await message.send(await getBuffer(url.result), {}, 'image');
+	return await message.send(await getBuffer(url.result.url), {}, 'image');
 });
